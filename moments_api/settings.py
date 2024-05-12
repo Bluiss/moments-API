7c0c1bfaef7869bfc04f622c6cb4dc7cbb8873d6
Bluiss <135335614+Bluiss@users.noreply.github.com>
2024-05-12 19:36:49 +0100

    update

--- moments_api/settings.py
+++ moments_api/settings.py
@@ -116,10 +116,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     'https://moments-hb-b9a64daf59f3.herokuapp.com'
 ]
-# Add local development origin if CLIENT_ORIGIN_DEV is set
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    dev_origin = os.environ.get('CLIENT_ORIGIN_DEV')
-    CORS_ALLOWED_ORIGINS.append(dev_origin)
 
 
 CORS_ALLOW_CREDENTIALS = True
