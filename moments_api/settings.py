7026a13b374cf2b9a32974ea8daa1994da815731
Harry Britton <harrydbritton@gmail.com>
2024-05-13 18:10:32 +0000

    update

--- moments_api/settings.py
+++ moments_api/settings.py
@@ -108,7 +108,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
